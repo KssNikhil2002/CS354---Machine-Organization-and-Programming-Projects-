ee1f5676c7306329a3850064d7629c2861bc15b4
KssNikhil2002 <82465617+KssNikhil2002@users.noreply.github.com>
2023-06-02 15:55:41 -0500

    Add files via upload

---
+++ project 4/cache1D.c
@@ -0,0 +1,14 @@
+//Kruthiventi Shyama Subrahmanya Nikhil
+
+#define GLOBAL_N 100000
+
+int arr[GLOBAL_N];
+
+int main()
+{
+	for(int i = 0;i < GLOBAL_N;i++)
+	{
+		arr[i] = i;
+	}
+	return 0;
+}
---
+++ project 4/cache2Dcols.c
@@ -0,0 +1,21 @@
+//Kruthiventi Shyama Subrahmanya Nikhil
+
+#define rows 3000
+#define cols 500
+
+int arr2D[rows][cols];
+
+int main()
+{
+        for(int col = 0;col < cols;col++)
+        {
+                for(int row = 0;row < rows;row++)
+                {
+                        arr2D[row][col] = row + col;
+                }
+        }
+        return 0;
+}
+
+
+
